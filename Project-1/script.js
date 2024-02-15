1baa8df9f43d8abb5e46c049c2f40957ef4f13bc
cyemaune <153061315+cyemaune@users.noreply.github.com>
2024-02-16 02:03:33 +0700

    Add files via upload

---
+++ Project-1/script.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to initialize custom dropdown style
+    function initializeDropdownStyle() {
+      var locationDropdown = document.getElementById("location");
+  
+      if (locationDropdown) {
+        locationDropdown.style.width = "100%";
+        // Add any additional styling you need here
+      }
+    }
+  
+    // Call the function to initialize custom dropdown style
+    initializeDropdownStyle();
+  });
+  
